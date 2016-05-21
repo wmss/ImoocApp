0f8dbd3a9e1f314fb6a7efc021874c56d948570b
wmss <wmss.wj.true@gmail.com>
2016-05-21 19:54:01 +0800

    加一个页面，ToggleButtonDemo

--- app/src/main/java/tk/wmss/imoocapp/MainActivity.java
+++ app/src/main/java/tk/wmss/imoocapp/MainActivity.java
@@ -1,7 +1,11 @@
 package tk.wmss.imoocapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import tk.wmss.imoocapp.demoapi.ToggleButtonDome;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toToggleButtonDome(View view){
+        Intent intent = new Intent(MainActivity.this, ToggleButtonDome.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/tk/wmss/imoocapp/demoapi/ToggleButtonDome.java
@@ -0,0 +1,18 @@
+package tk.wmss.imoocapp.demoapi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import tk.wmss.imoocapp.R;
+
+/**
+ * Created by wmss_ on 2016/5/21.
+ */
+public class ToggleButtonDome extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_toggle_button_demo);
+    }
+}
