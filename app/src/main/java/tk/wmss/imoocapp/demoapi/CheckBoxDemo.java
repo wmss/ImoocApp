ecde633730d4c1e8302603f8ee79532258ca6d4e
wmss <wmss.wj.true@gmail.com>
2016-05-24 17:10:21 +0800

    0.0

--- app/src/main/java/tk/wmss/imoocapp/demoapi/CheckBoxDemo.java
+++ app/src/main/java/tk/wmss/imoocapp/demoapi/CheckBoxDemo.java
@@ -14,6 +14,7 @@ import tk.wmss.imoocapp.R;
  * 以下类还未测试是否能成功运行，明天再说，睡觉！！！
  */
 public class CheckBoxDemo extends AppCompatActivity {
+    private CheckBox checkBox ;
     private String texts = "你选择的英雄有：";
 
     @Override
@@ -45,7 +46,7 @@ public class CheckBoxDemo extends AppCompatActivity {
     * 该方法判断当前输入的CheckBox是否为true,如果为true则将当前的CheckBox的text属性值加入到texts变量值
     * */
     public void addS(int id) {
-        final CheckBox checkBox = (CheckBox) findViewById(id);
+        checkBox = (CheckBox) findViewById(id);
         if (checkBox != null)
             checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
